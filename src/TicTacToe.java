cecff7d6f1031951e4cbcdcbe3e7534c7c7f679c
Calvin Woolheater <cwoolhea@asu.edu>
2025-11-09 13:54:59 -0600

    Add printBoard placeholder and temporary call for demo

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,5 +2,13 @@ public class TicTacToe {
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe!");
         // TODO: Implement game logic
+        printBoard(); // temporary call for demo
+    }
+
+    // Demo-only placeholder
+    static void printBoard() {
+        System.out.println("[ ][ ][ ]");
+        System.out.println("[ ][ ][ ]");
+        System.out.println("[ ][ ][ ]");
     }
 }
